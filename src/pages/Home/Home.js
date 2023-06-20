723538dbaa586974074182b49c2e63b715cb75a6
Neha Bhole <nehasbhole@gmail.com>
2023-06-20 12:24:01 -0400

    add headline component to home?

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import "./Home.scss";
 import Header from "../../components/Header/Header";
 import KeyFeature from "../../components/KeyFeature/KeyFeature";
 import HomeText from "./HomeText";
+import Headline from "../../components/Headline/Headline";
 
 const Home = () => {
   return (
@@ -9,7 +10,7 @@ const Home = () => {
       <Header />
       <main>
         {HomeText.headline.map(({ icon, text }, i) => (
-          <KeyFeature key={i} text={text} icon={icon} />
+          <Headline key={i} text={text} icon={icon} />
         ))}
 
         {HomeText.features.map(({ icon, text }, i) => (
