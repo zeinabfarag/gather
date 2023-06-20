8cad614d533e3a5b67f88f97b3c1421bd73416e2
zeinabfarag <zeinabfarag454@gmail.com>
2023-06-20 12:16:29 -0400

    top landing

--- src/pages/Home/HomeText.js
+++ src/pages/Home/HomeText.js
@@ -10,7 +10,7 @@ const data = {
     },
     {
       icon: "idk",
-      text: "<b> Create spontaneous </b> moments with your loved ones",
+      text: "<b> Create </b> spontaneous moments with your loved ones",
     },
   ],
 };
