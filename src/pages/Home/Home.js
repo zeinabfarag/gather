4e8da524a71b1ac5a7c2e364c6716a3955c40ba5
zeinabfarag <zeinabfarag454@gmail.com>
2023-06-20 11:46:36 -0400

    hometext

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,14 @@
 import "./Home.scss";
+import KeyFeature from "../../KeyFeature/KeyFeature";
+import HomeText from "./HomeText";
 
 const Home = () => {
   return (
     <div className="App">
       <h1>Gather</h1>
+      {HomeText.features.map(({ icon, text }, i) => (
+        <KeyFeature key={i} text={text} icon={icon} />
+      ))}
     </div>
   );
 };
---
+++ src/pages/Home/HomeText.js
@@ -0,0 +1,18 @@
+const data = {
+  features: [
+    {
+      icon: "idk",
+      text: " <b> Customize </b> your schedule sync with unique social circles.",
+    },
+    {
+      icon: "idk",
+      text: "<b> Plan </b> the entire hang out in app, and directly <b> sync </b>  it to your calendar",
+    },
+    {
+      icon: "idk",
+      text: "<b> Create spontaneous </b> moments with your loved ones",
+    },
+  ],
+};
+
+export default data;
